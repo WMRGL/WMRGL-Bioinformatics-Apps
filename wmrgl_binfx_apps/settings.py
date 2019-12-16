98fb587c28d6eb06dce124c857adf033e650e474
Theo Cole <theo.j.cole@gmail.com>
2019-12-16 11:34:12 +0000

    modify settings to include config

--- wmrgl_binfx_apps/settings.py
+++ wmrgl_binfx_apps/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+from wmrgl_binfx_apps.config import *
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
